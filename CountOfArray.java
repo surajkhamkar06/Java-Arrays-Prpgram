4ad4dd7b504523268c75dc0b86143fb5f29b180d
Khamkar Suraj <135463570+surajkhamkar06@users.noreply.github.com>
2025-07-29 22:44:23 +0530

    Count Of Arrays

---
+++ CountOfArray.java
@@ -0,0 +1,18 @@
+package package1;
+
+public class CountOfArray 
+{
+	public static void main(String[] args) 
+	{
+		int a [] = {20, 54, 78, 8, 96, 4};
+		int b = 0;
+
+		for(int i=0; i<a.length; i++)
+		{
+			//System.out.print(a[i] + " ");
+		}
+		System.out.println(a.length);
+
+	}
+
+}
