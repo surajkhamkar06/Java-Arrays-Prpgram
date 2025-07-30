c5d2fbaf87a7522955dfa6c06a7546d87de581b4
Khamkar Suraj <135463570+surajkhamkar06@users.noreply.github.com>
2025-07-30 23:33:56 +0530

    Find the odd number of given array

---
+++ OddNumber.java
@@ -0,0 +1,19 @@
+package package1;
+
+public class OddNumber 
+{
+	public static void main(String[] args)
+	{
+		int a [] = {20, 54, 77, 8, 96, 5};
+	
+		for(int i=0; i<a.length; i++)
+		{
+			if(a[i]%2!=0)
+			{
+				System.out.print(a[i] + " ");
+			}
+		}
+		
+	}
+
+}
